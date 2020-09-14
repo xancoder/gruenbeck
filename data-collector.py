3ecdff48b92100d1ab85d76a035f15568f4802ed
Alexander Sommer <xancoder@gmail.com>
2020-09-14 09:18:11 +0200

    feat(config): add configuration
    * add json configuration file with static name "config.json" in main script folder
    * add method to check and read configuration file

--- data-collector.py
+++ data-collector.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import json
+import pathlib
+
+
 def main():
-    pass
+    config = get_configuration()
+
+
+def get_configuration():
+    config_p = pathlib.Path("config.json")
+    if not config_p.exists():
+        print(f"[-] config file not found: {config_p}")
+    else:
+        print(f"[*] read config file: {config_p}")
+        with config_p.open() as json_data_file:
+            config = json.load(json_data_file)
+    return config
 
 
 if __name__ == '__main__':
