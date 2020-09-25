aa0a3ae4c8557f1aba6a0c47e5dadc3391eb1732
Alexander Sommer <xancoder@gmail.com>
2020-09-25 13:45:01 +0200

    test(configuration-file): correct imports and add required properties tests

--- tests/datacollector/test_configuration_file.py
+++ tests/datacollector/test_configuration_file.py
@@ -1,6 +1,6 @@
 import pytest
 
-import src.datacollector
+import datacollector
 
 CONFIGURATION_FILE = '../src/config.json'
 
@@ -8,9 +8,44 @@ CONFIGURATION_FILE = '../src/config.json'
 def test_check_configuration_file_not_exists():
     configuration_file_nonsense = 'example.json'
     with pytest.raises(FileNotFoundError):
-        config = src.datacollector.check_configuration(configuration_file_nonsense)
+        config = datacollector.check_configuration(configuration_file_nonsense)
 
 
 def test_check_configuration_file_type():
-    config = src.datacollector.check_configuration(CONFIGURATION_FILE)
+    config = datacollector.check_configuration(CONFIGURATION_FILE)
     assert isinstance(config, dict)
+
+
+def test_check_configuration_parameter_not_exists():
+    item = 'example'
+    config = datacollector.check_configuration(CONFIGURATION_FILE)
+    with pytest.raises(KeyError):
+        tmp = config[item]
+
+
+def test_check_configuration_parameter_parameter_file():
+    item = 'parameterFile'
+    config = datacollector.check_configuration(CONFIGURATION_FILE)
+    tmp = config[item]
+    assert isinstance(tmp, str)
+
+
+def test_check_configuration_parameter_data_path():
+    item = 'dataPath'
+    config = datacollector.check_configuration(CONFIGURATION_FILE)
+    tmp = config[item]
+    assert isinstance(tmp, str)
+
+
+def test_check_configuration_parameter_data_file():
+    item = 'dataFile'
+    config = datacollector.check_configuration(CONFIGURATION_FILE)
+    tmp = config[item]
+    assert isinstance(tmp, dict)
+
+
+def test_check_configuration_parameter_softwatersystem():
+    item = 'softWaterSystem'
+    config = datacollector.check_configuration(CONFIGURATION_FILE)
+    tmp = config[item]
+    assert isinstance(tmp, dict)
