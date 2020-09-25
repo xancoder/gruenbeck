60e44781e5a9a4f82711c9459b3fad9538b3a4e8
Alexander Sommer <xancoder@gmail.com>
2020-09-25 13:43:41 +0200

    test(data-folder): correct imports

--- tests/datacollector/test_data_folder.py
+++ tests/datacollector/test_data_folder.py
@@ -1,18 +1,18 @@
 import pathlib
 
-import src.datacollector
+import datacollector
 
 DATA_FOLDER = '../data'
 
 
 def test_check_data_folder_not_exists():
     data_folder_nonsense = './folderName'
-    data_path = src.datacollector.check_data_folder(data_folder_nonsense)
-    assert data_path.exists() == True
+    data_path = datacollector.check_data_folder(data_folder_nonsense)
+    assert data_path.exists()
     # clean up
     data_path.rmdir()
 
 
 def test_check_data_folder_exists():
-    data_path = src.datacollector.check_data_folder(DATA_FOLDER)
+    data_path = datacollector.check_data_folder(DATA_FOLDER)
     assert isinstance(data_path, pathlib.Path)
